cf6fadaf93afa84a388fd611038785c0afc61849
Paulo Sampaio <paulo.sampaio@stoodi.com.br>
2020-07-22 21:01:34 -0300

    fixing get_random_quote() random upper limit. setting open mode on all open()

--- get_quote.py
+++ get_quote.py
@@ -1,14 +1,14 @@
 import random
 
 def read_quotes_from_file(file):
-    qf = open(file)
+    qf = open(file, 'r')
     quotes = qf.readlines()
     qf.close()
     return quotes
 
 def get_random_quote():
     quotes = read_quotes_from_file('quotes.txt')
-    rnd = random.randint(0, len(quotes))
+    rnd = random.randint(0, len(quotes)-1)
     return quotes[rnd]
 
 if __name__== "__main__":
